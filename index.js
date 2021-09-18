1ad433f4e90f07c06dbc057ff0b2c3e559d94eed
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-18 23:27:20 +0530

    Rename Images and Add Button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // Random Number Generation 1 - 6
-var randomNumber1 = Math.floor(Math.random()*6 + 1);
-var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var randomNumber1 = Math.floor(Math.random() * 6 )+ 1;
+var randomNumber2 = Math.floor(Math.random() * 6 )+ 1;
 
 //random dice image
 var randomDiceImage1 = "./images/dice" + randomNumber1 + ".png"
@@ -9,20 +9,24 @@ var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png"
 
 
 //Setting Source to image tags
-document.querySelector("#img1").setAttribute("src",randomDiceImage1);
-document.querySelector("#img2").setAttribute("src",randomDiceImage2);
+document.querySelector("#img1").setAttribute("src", randomDiceImage1);
+document.querySelector("#img2").setAttribute("src", randomDiceImage2);
 
 
 // Dynamic Title
-if(randomNumber1 > randomNumber2)
-{
-    document.querySelector("#title").textContent="Player 1 Wins";
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("#title").textContent = "Player 1 Wins";
 }
-else if(randomNumber1 == randomNumber2)
-{
-    document.querySelector("#title").textContent="It's a Tie";
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("#title").textContent = "Player 2 Wins";
 }
-else
-{
-    document.querySelector("#title").textContent="Player 2 Wins";
+else {
+    document.querySelector("#title").textContent = "It's a Tie";
+}
+
+// Reset Page
+function myFunction() {
+    document.querySelector("#title").textContent = "Refresh Me";
+    document.querySelector("#img1").setAttribute("src", "./images/dice6.png" );
+    document.querySelector("#img2").setAttribute("src", "./images/dice6.png" );
 }
