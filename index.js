be15fc871d5620bcc248ea7eaf61a04fe28f84ee
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-18 21:36:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// Random Number Generation 1 - 6
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+
+//random dice image
+var randomDiceImage1 = "/images/Dice-" + randomNumber1 + ".png"
+var randomDiceImage2 = "/images/Dice-" + randomNumber2 + ".png"
+
+
+//Setting Source to image tags
+document.querySelector("#img1").setAttribute("src",randomDiceImage1);
+document.querySelector("#img2").setAttribute("src",randomDiceImage2);
+
+
+// Dynamic Title
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("#title").textContent="Player 1 Wins";
+}
+else if(randomNumber1 == randomNumber2)
+{
+    document.querySelector("#title").textContent="It's a Tie";
+}
+else
+{
+    document.querySelector("#title").textContent="Player 2 Wins";
+}
